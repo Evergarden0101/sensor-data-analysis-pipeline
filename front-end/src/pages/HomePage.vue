8a02fae688f035d356b35496f589a3d47cb7c483
epura <eleonora_pura@hotmail.it>
2023-07-17 16:51:06 +0200

    created new progressbar for HomePage.vue

--- front-end/src/pages/HomePage.vue
+++ front-end/src/pages/HomePage.vue
@@ -1,89 +1,35 @@
 <template>
-    <body>
-        <div class="container">
-        <div class="steps">
-            <span class="circle">Data upload</span>
-            <span class="circle">Sleep Stage Classification</span>
-            <span class="circle">Brux Classification Dashboard</span>
-            <span class="circle">Brux Events Monitoring</span>
-        </div>
-        <div class="buttons">
-            <router-link :to="'/upload/'" style="margin-right: 20px;">
-                <button class="button">Start</button>
-            </router-link>
-        </div>
-        </div>
-    </body>
-  </template>
-  
-  <style>
-  /* Import Google font - Poppins */
-  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap");
-  * {
-    margin: 0;
-    padding: 0px;
-    box-sizing: border-box;
-    font-family: "Poppins", sans-serif;
-  }
-  body {
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: #f6f7fb;
-  }
-  .container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 40px;
-    max-width: 400px;
-    width: 100%;
-  }
-  .container .steps {
-    display: flex;
-    width: 100%;
-    align-items: center;
-    justify-content: space-between;
-    position: relative;
-  }
-  .steps .circle {
-    margin-right: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 80px;
-    width: 80px;
-    color: #4070f4;
-    font-size: 22px;
-    font-weight: 500;
-    border-radius: 50%;
-    background: #fff;
-    border: 4px solid #4070f4;
-    transition: all 200ms ease;
-    transition-delay: 0s;
-    font-size: 10px;
-    text-align: center;
-  }
-  
-  .container .buttons {
-    display: flex;
-    gap: 20px;
-  }
-  .buttons button {
-    padding: 8px 25px;
-    background: #4070f4;
-    border: none;
-    border-radius: 8px;
-    color: #fff;
-    font-size: 16px;
-    font-weight: 400;
-    cursor: pointer;
-    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.05);
-    transition: all 200ms linear;
-  }
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  </style>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row mb-5">
+        <h1>Welcome to the sensor data pipeline for the labelling and monitoring of bruxism events!</h1>
+      </div>
+      <div class="row align-items-center justify-content-center mb-5 mt-5">
+        <div class="col-12">
+          <ul class="progressbar">
+            <li class="">Data upload</li>
+            <li class="">Sleep Stage Classification</li>
+            <li class="">Brux Classification Dashboard</li>
+            <li class="">Brux Events Monitoring</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-12 text-center">
+        <button type="button" class="btn btn-primary">Start</button>
+      </div>
+    </div>
+  </body>
+</template>
 
 <script>
 export default {
@@ -93,4 +39,56 @@ export default {
     },
 };
 </script>
+
+<style>
+.progressbar{
+  counter-reset: step;
+
+}
+.progressbar li {
+  list-style-type: none;
+  float: left;
+  width: 25%;
+  position: relative;
+  text-align: center;
+}
+.progressbar li:before{
+  content: counter(step);
+  counter-increment: step;
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  border: 2px solid #ddd;
+  display: block;
+  text-align: center;
+  margin: 0 auto 10px auto;
+  border-radius: 50%;
+  background-color: white;
+
+}
+.progressbar li:after{
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  background-color: #ddd;
+  top: 20px;
+  left:-50%;
+  z-index:-1;
+}
+.progressbar li:first-child:after{
+  content: none;
+}
+.progressbar li.active{
+  color:darkblue;
+}
+.progressbar li.active:before{
+  border-color: darkblue;
+}
+.progressbar li.active + li:after{
+  background-color: darkblue;
+}
+
+
+</style>
   
