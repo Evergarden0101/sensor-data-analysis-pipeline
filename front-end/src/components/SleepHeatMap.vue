ce507de251da361a7bc8fd243a1a7cad3fa733d4
epura <eleonora_pura@hotmail.it>
2023-08-11 13:07:52 +0200

    first "working" version of HRV integration

--- front-end/src/components/SleepHeatMap.vue
+++ front-end/src/components/SleepHeatMap.vue
@@ -39,9 +39,17 @@ export default {
     }
   },
   mounted() {
-    this.drawTreatHeatMap();
+    this.drawTreatHeatMap(1, 1, 1222325);
   },
   methods: {
+    getMax(arr, prop) {
+        var max;
+        for (var i=0 ; i<arr.length ; i++) {
+            if (max == null || parseInt(arr[i][prop]) > parseInt(max[prop]))
+                max = arr[i];
+        }
+        return max;
+    },
     getEcgData(patient_id, week, night_id){
         const path = `http://localhost:5000/hrv-features/${patient_id}/${week}/${night_id}`
         const headers = { 
@@ -59,129 +67,139 @@ export default {
         })
 
     },
-    async drawTreatHeatMap(){
+    drawTreatHeatMap(patient_id, week, night_id){
         var dom = document.getElementById("chart-container");
         var myChart = echarts.init(dom, null, {
             renderer: "sgv",
             useDirtyRect: false
         });
 
-        var option;
+        const path = `http://localhost:5000/hrv-features/${patient_id}/${week}/${night_id}`
+        const headers = { 
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            };
 
-        var minutes = [5, 10 , 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90];
-        var hours = ['1.5h', '3h', '4.5h',
-                '6h', '7.5h', '9h', '10.5h', '12h'];
-        /*
-        if(this.dataReceived===true){
-           var data = this.ecgData; 
-        }
-        else{
-            await this.getEcgData(1, 1, 1222325);
-            data = this.ecgData;
-        }
-        */
-        /*
-        var data = [{'LF/HF': 2.0107152456813915,
-            'SD': 290.6453341073007,
-            'end_id': 300000,
-            'stage': "nrem",
-            'start_id': 0,
-            'x': 0,
-            'y': 0
-        },
-        {
-            'LF/HF':1.2472080864024337,
-            'SD': 172.26272979069515,
-            'end_id': 600000,
-            'stage': "rem",
-            'start_id': 300000,
-            'x': 1,
-            'y': 0
-        }, {
-            'LF/HF':1.8449693678005492,
-            'SD': 153.53789804010702,
-            'end_id': 900000,
-            'stage': "rem",
-            'start_id': 600000,
-            'x': 2,
-            'y': 0
-        }
-        ];
-        */
+        axios.get(path, {headers})
+            .then((res) => {
+            console.log("Data received");
+            var option;
 
-        await this.getEcgData(1, 1, 1222325);
-
-
-        var data = this.ecgData.map(function (item) {
-            return [item['x'], item['y'], item['SD'], item['stage']];
-        });
-
-        option = {
-            tooltip: {
-                position: 'top'
+            var minutes = [5, 10 , 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90];
+            var hours = ['1.5h', '3h', '4.5h',
+                    '6h', '7.5h', '9h', '10.5h', '12h'];
+            /*
+            if(this.dataReceived===true){
+            var data = this.ecgData; 
+            }
+            else{
+                await this.getEcgData(1, 1, 1222325);
+                data = this.ecgData;
+            }
+            */
+            /*
+            var data = [{'LF/HF': 2.0107152456813915,
+                'SD': 290.6453341073007,
+                'end_id': 300000,
+                'stage': "nrem",
+                'start_id': 0,
+                'x': 0,
+                'y': 0
             },
-            animation: false,
-            grid: {
-                height: '50%',
-                top: '10%'
-            },
-            xAxis: {
-                type: 'category',
-                name: 'minutes',
-                data: minutes,
-                splitArea: {
-                    show: true
-                }
-            },
-            yAxis: {
-                type: 'category',
-                data: hours,
-                name: 'hours',
-                splitArea: {
-                    show: true
-                }
-            },
-            visualMap: {
-                min: 0,
-                max: 10,
-                dimension: 2,
-                inRange : {   
-                    color: ['#D3D3D3', '#009000'] //From smaller to bigger value ->
+            {
+                'LF/HF':1.2472080864024337,
+                'SD': 172.26272979069515,
+                'end_id': 600000,
+                'stage': "rem",
+                'start_id': 300000,
+                'x': 1,
+                'y': 0
+            }, {
+                'LF/HF':1.8449693678005492,
+                'SD': 153.53789804010702,
+                'end_id': 900000,
+                'stage': "rem",
+                'start_id': 600000,
+                'x': 2,
+                'y': 0
+            }
+            ];
+            */
+
+            var data = res.data.map(function (item) {
+                return [item['x'], item['y'], item['SD'], item['stage']];
+            });
+
+            option = {
+                tooltip: {
+                    position: 'top'
                 },
-                calculable: true,
-                orient: 'horizontal',
-                left: 'center',
-                bottom: '15%',
-            },
-            series: [{
-                name: 'Punch Card',
-                type: 'heatmap',
-                data: data,
-                label: {
-                    show: true,
-                    formatter: (param) => {
-                        if (param.data[3]  === 'rem'){
-                            return 'REM'
-                        }
-                        else{
-                            return ''
-                        }
-                    
+                animation: false,
+                grid: {
+                    height: '50%',
+                    top: '10%'
+                },
+                xAxis: {
+                    type: 'category',
+                    name: 'minutes',
+                    data: minutes,
+                    splitArea: {
+                        show: true
                     }
                 },
-                emphasis: {
-                    itemStyle: {
-                        shadowBlur: 10,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                yAxis: {
+                    type: 'category',
+                    data: hours,
+                    name: 'hours',
+                    splitArea: {
+                        show: true
                     }
-                }
-            }]
-        };
-        if (option && typeof option === "object") {
-            myChart.setOption(option);
-        }
+                },
+                visualMap: {
+                    min: 0,
+                    max: this.getMax(res.data, "SD").SD,
+                    dimension: 2,
+                    inRange : {   
+                        color: ['#D3D3D3', '#009000'] //From smaller to bigger value ->
+                    },
+                    calculable: true,
+                    orient: 'horizontal',
+                    left: 'center',
+                    bottom: '15%',
+                },
+                series: [{
+                    name: 'Punch Card',
+                    type: 'heatmap',
+                    data: data,
+                    label: {
+                        show: true,
+                        formatter: (param) => {
+                            if (param.data[3]  === 'rem'){
+                                return 'REM'
+                            }
+                            else{
+                                return ''
+                            }
+                        
+                        }
+                    },
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }]
+            };
+            if (option && typeof option === "object") {
+                myChart.setOption(option);
+            }
 
-        window.addEventListener("resize", myChart.resize);
+            window.addEventListener("resize", myChart.resize);
+        })
+        .catch(err=>{
+            console.log(err)
+        })
     }
 }
 }
