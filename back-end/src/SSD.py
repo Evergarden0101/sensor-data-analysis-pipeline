5af04e72e0b824f22ad96b1cfb3bbd98d8295237
epura <eleonora_pura@hotmail.it>
2023-09-20 08:52:12 +0200

    corrected original script bugs

--- back-end/src/SSD.py
+++ back-end/src/SSD.py
@@ -1,10 +1,11 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
-import datetime
+import datetime, math
 import neurokit2 as nk
 import hrvanalysis as hrvana
 from preprocessing import *
 from settings import *
+
 
 
 # FUNCTIONS
@@ -23,11 +24,20 @@ def find_rr_intervals(cleaned_ecg):
 def clean_rr_intervals(hrv_df):
     clean_rri = hrv_df['RR Intervals'].values
     clean_rri = hrvana.remove_outliers(rr_intervals=clean_rri, low_rri=300, high_rri=2000)
+    print(f"Indices of nan values 1: {np.argwhere(np.isnan(clean_rri))}")
     clean_rri = hrvana.interpolate_nan_values(rr_intervals=clean_rri, interpolation_method="linear")
     clean_rri = hrvana.remove_ectopic_beats(rr_intervals=clean_rri, method="malik")
     clean_rri = hrvana.interpolate_nan_values(rr_intervals=clean_rri, interpolation_method="linear")
+    print(f"Indices of nan values 2: {np.argwhere(np.isnan(clean_rri))}")
+    # If there are still nan values, remove them with further interpolation
+    clean_rri = np.array(clean_rri)
+    nans, x= nan_helper(clean_rri)
+    clean_rri[nans]= np.interp(x(nans), x(~nans), clean_rri[~nans])
+    print("check for null values in cleaned rri intervals")
+    print(np.isnan(clean_rri).any())
 
-    return clean_rri
+    return clean_rri.tolist()
+    #return clean_rri
 
 
 """Return the REM and NREM LF/HF ranges from: Herzig et al., Reproducibility of Heart Rate Variability Is Parameter and Sleep Stage Dependent (2017).
@@ -68,53 +78,89 @@ def get_ako_ranges():
 - SAMPLING_RATE: desired sampling rate (default: 1000)
 """
 
-def get_HRV_features(patient_id, week, night_id, n,  SAMPLING_RATE=SAMPLING_RATE):
+def get_HRV_features(patient_id, week, night_id,  SAMPLING_RATE):
     start = 0
+    print(f"start: {start}")
     # Number of data points in 5 minutes
-    window_size = INTERVAL*60*SAMPLING_RATE
+    window_size = 5*60*SAMPLING_RATE
+    print(f"window size: {window_size}")
     end = window_size
+    print(f"end: {end}")
     values = []
     y=0
     x=0
-    
+
     # Open right CSV file and get ECG data
-    df = open_brux_csv(patient_id=patient_id, week=week, night_id=night_id)
-    ecg = get_column_data_from_df(df, "ECG")
-    ecg_array = get_column_array(ecg)
+    df = open_brux_csv(patient_id, week, night_id)
+    print(f"dfshape: {df.shape}")
+    ecg = df["ECG"]
+    ecg_array = ecg.values.tolist()
+    print(f"last element: {ecg_array[-1]}")
+    print(f" ECG Array length: {len(ecg)}")
 
-    # Downsample to 1000Hz
-    ecg_ds = resample_signal(signal=ecg_array)
 
-    # Select first n minutes
-    ecg_nmin = get_n_minutes(ecg_ds, n, SAMPLING_RATE=SAMPLING_RATE)
-    print(f"Signal duration: {get_signal_duration(ecg_nmin)}")
-    
+    print(f"ECG Array duration: {datetime.timedelta(seconds=(len(ecg_array)/SAMPLING_RATE))}")
 
-    while end <= len(ecg_nmin)+1:
+
+    print(f"ECG Array duration in minutes: {(datetime.timedelta(seconds=(len(ecg_array)/SAMPLING_RATE))).total_seconds()/60}")
+
+    signal_duration_minutes = (datetime.timedelta(seconds=(len(ecg_array)/SAMPLING_RATE))).total_seconds() / 60
+        
+    n = math.floor(signal_duration_minutes)
+
+    while n%5!=0:
+        n = n-1
+
+    print(f"Adapted minutes duration: {n}")
+
+    data_points = n*60*SAMPLING_RATE
+    print(f"New number of data points: {data_points}")
+    ecg_nmin = ecg_array[:data_points]
+    print(f"rounded down array length: {len(ecg_nmin)}")
+    print(f"last element: {ecg_nmin[-1]}")
+
+    print(f"Adapted Signal duration: {datetime.timedelta(seconds=(len(ecg_nmin)/SAMPLING_RATE))}")
+
+    while end <= len(ecg_nmin):
+        print("Beginning of while loop")
+        print(f"end:{end}")
+        print(f"length of data: {len(ecg_nmin)}")
         stage = ''
 
         # 5 minutes of ecg data
         ecg = ecg_nmin[start:end]
 
+        print(f"5 minutes of data: {len(ecg)}")
+        print(f"last element of the 5 minutes: {ecg[-1]}")
+
         # Filter the data with ranges specified from Barbara
         filter_ecg = nk.signal_filter(ecg, sampling_rate=SAMPLING_RATE, lowcut=0.5, highcut=150)
 
+
         # Clean ECG data
         cleaned = nk.ecg_clean(filter_ecg, sampling_rate=SAMPLING_RATE, method="pantompkins1985")
+
+        print(f"cleaned isna(): {np.isnan(cleaned).any()}")
 
         #Find RR intervals    
         hrv_df = find_rr_intervals(cleaned)
 
         # Clean RR intervals
-    
+
         clean_rri = clean_rr_intervals(hrv_df)
         hrv_df["RR Intervals"] = clean_rri 
 
         # HRV feature extraction
         nn_epoch = hrv_df['RR Intervals'].values
 
+        print(type(nn_epoch))
+
+        print(f"are there nullvalues: {hrv_df['RR Intervals'].isna().any()}")
+
         time_features = hrvana.get_time_domain_features(nn_epoch)
+        print(f"time_features: {time_features}")
         frequency_features = hrvana.get_frequency_domain_features(nn_epoch)
+        print(f"frequency features: {frequency_features}")
 
         print(f"LF/HF ratio: {frequency_features['lf_hf_ratio']}")
         nrem = get_herzig_ranges()['nrem']
@@ -142,6 +188,7 @@ def get_HRV_features(patient_id, week, night_id, n,  SAMPLING_RATE=SAMPLING_RATE
             'x': x
         })
 
+        print(f"values appended: {values[-1]}")
         # Calculate the coordinates for the SleepHeatMap.vue
         if x==((SLEEP_CYCLE/INTERVAL)-1):
             x=0
@@ -152,5 +199,7 @@ def get_HRV_features(patient_id, week, night_id, n,  SAMPLING_RATE=SAMPLING_RATE
 
         start = end
         end += window_size
-    
+        print(f"start after increase: {start}")
+        print(f"end after increase: {end}")
+
     return values
