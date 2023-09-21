4e0daeee3066cc2c42c299b66aca105f050cff70
epura <eleonora_pura@hotmail.it>
2023-09-21 09:05:11 +0200

    removed useless setting

--- back-end/src/settings.py
+++ back-end/src/settings.py
@@ -1,6 +1,5 @@
 # GLOBAL VARIABLES
 INTERVAL = 5 # We want to take data at every 5 minutes interval
-ORIGINAL_SAMPLING_RATE = 2000
 SLEEP_CYCLE = 90
 DATA_PATH = "C:/Users/eleon/Desktop/Master/FJS 23/Master Project/sensor-data-analysis-pipeline/back-end/src/data/"
 #DATA_PATH = "C:/Users/Karim/Desktop/UniZH/Master/Not Done/Master Project/sensor-data-analysis-pipeline/back-end/src/data"
