fbf410f4bd1fdc29ef9a91411bce3d9f785c700c
epura <eleonora_pura@hotmail.it>
2023-10-04 19:58:17 +0200

    renaming

--- front-end/src/components/Stepper.vue
+++ front-end/src/components/Stepper.vue
@@ -2,7 +2,7 @@
     <el-steps :active=step finish-status="process">
         <el-step title="Patient Information" description="Provide the patient information" />
         <!--<el-step title="Select patient" description="Select the desired patient and the desired day" />-->
-        <el-step title="Sleep Stage Classification" description="Label sleep stages based on HRV analysis" />
+        <el-step title="Sleep Stage Detection" description="Label sleep stages based on HRV analysis" />
         <el-step title="Events Classification Dashboard" description="Check for specific events in the data and label them" />
         <el-step title="Events Monitoring" description="Compare patients data in different days and weeks" />
     </el-steps>
