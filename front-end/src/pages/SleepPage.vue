c41de001ec10314b485dbda2c9ed471773fee95f
epura <eleonora_pura@hotmail.it>
2023-10-05 15:26:04 +0200

    restyled sleep page

--- front-end/src/pages/SleepPage.vue
+++ front-end/src/pages/SleepPage.vue
@@ -1,19 +1,28 @@
 <template>
-    <el-row style="margin-bottom: 2%;">
-        <el-col>
+
+<div class="common-layout">
+    <el-container>
+      <el-container>
+        <el-header>
             <h1 style="text-align: center;">This is the Sleep Stage Detection page!</h1>
-        </el-col>
-    </el-row>
-    <el-row>
-        <el-col align-center :span="12" :offset="6">
-            <Stepper step=2 />
-        </el-col>
-    </el-row>
-    <el-row style="margin-top: 5%;">
-        <el-col>
+            <el-row style="margin-top: 2%;">
+                <el-col :span="14" :offset="6">
+                   <Stepper step=2 /> 
+                </el-col>
+                
+            </el-row>
+            
+
+            <h2 align="center" style="margin-top: 3%;">Patient: {{ this.$store.state.patientId }}, Week: {{ this.$store.state.week }}, Night id: {{ this.$store.state.nightId }}</h2>
+            <h3 align="center">Threshold Filtering</h3>
+        </el-header>
+        <el-main>
             <SleepHeatMap />
-        </el-col>
-    </el-row>
+        </el-main>
+      </el-container>
+      <el-aside width="400px" style="margin-top: 18%; margin-right: 3%;"><SleepHeatMapLegend /></el-aside>
+    </el-container>
+</div>
 
 </template>
 
@@ -21,11 +30,13 @@
 <script>
 import Stepper from '@/components/Stepper.vue';
 import SleepHeatMap from '@/components/SleepHeatMap.vue';
+import SleepHeatMapLegend from '@/components/SleepHeatMapLegend.vue'
 export default {
     name: 'SleepPage',
     components: {
         Stepper,
-        SleepHeatMap
+        SleepHeatMap,
+        SleepHeatMapLegend
     },
 };
 </script>
