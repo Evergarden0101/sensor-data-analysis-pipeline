79ae0473ffea9ff8da455253eac42f17cf303953
epura <eleonora_pura@hotmail.it>
2023-10-09 16:46:56 +0200

    base structure TreatmentPage.vue

--- front-end/src/pages/TreatmentPage.vue
+++ front-end/src/pages/TreatmentPage.vue
@@ -16,153 +16,53 @@
             </router-link>
         </el-col>
     </el-row>
-    <p style="text-align: center; margin-top: 2%;">Please select a patient id to visualize mutiple weekly summary:</p>
-    <el-row style="margin-top: 1%;">
+
+    <el-row style="margin-top: 3%;">
         <el-col :offset="10">
-            <el-form :model="form" label-width="100px">
-                <el-form-item label="Patient UID">
-                    <el-autocomplete
-                        v-model="form.patientId"
-                        :fetch-suggestions="querySearch"
-                        clearable
-                        class="inline-input w-50"
-                        placeholder="Input Patient UID"
-                        @select="handleSelect"
-                        style="overflow: visible;"
-                    >
-                        <template #default="{ item }">
-                        <span
-                            style="
-                            float: left;
-                            font-size: 15px;
-                            "
-                            >{{ item.id }}</span>
-                        <span style="color: var(--el-text-color-secondary);float: right">UID</span>
-                        </template>
-                    </el-autocomplete>
-                </el-form-item>
-            </el-form>
+            <el-radio-group v-model="radio1" size="large">
+                <el-radio-button label="Day" />
+                <el-radio-button label="Week" />
+                <el-radio-button label="Month" />
+            </el-radio-group>
         </el-col>
     </el-row>
-    <div v-if="form.patientId">
-        <el-row style="margin-top: 4%;" class="row-bg" justify="space-evenly">
-            <el-col :span="7">
-                <TreatHeatMapW1 />
-            </el-col>
-            <el-col :span="7">
-                <TreatHeatMapW2 />
-            </el-col>
-            <el-col :span="7">
-                <TreatHeatMapW3 />
-            </el-col>
-        </el-row>
+
+    <div style="margin-top: 3%;">
+        <div v-if="radio1==='Day'" style="text-align: center;">
+            Day
+        </div>
+
+        <div v-if="radio1==='Week'" style="text-align: center;">
+            Week
+        </div>
+
+        <div v-if="radio1==='Month'" style="text-align: center;">
+            Month
+        </div>
+    </div>
+
+    <div style="border-style: solid; border-color: black; border-width:3px; padding: 40px; text-align: center; width:50%; margin: auto;">
+        <el-button>
+            Add patient
+        </el-button>
     </div>
 </template>
 
 
 <script>
 import Stepper from '@/components/Stepper.vue';
-import TestButton from '@/components/TestButton.vue';
-import TreatHeatMapW2 from '../components/TreatHeatMapW2.vue';
-import TreatHeatMapW1 from '@/components/TreatHeatMapW1.vue';
-import TreatHeatMapW3 from '@/components/TreatHeatMapW3.vue';
+import { ref } from 'vue';
 
 export default {
     name: 'TreatmentPage',
     components: {
         Stepper,
-        TestButton,
-        TreatHeatMapW2,
-        TreatHeatMapW1,
-        TreatHeatMapW3
 
     },
-    data() {
-        return {
-            form: {
-                patientId: '',
-                day: '',
-            },
-            ids:[],
-            props : { expandTrigger: 'hover'},
-            days : [{
-                value: 'Week1',
-                label: 'Week 1',
-                children: [
-                {
-                    value: 'Day1',
-                    label: 'Day 1',
-                    disabled: true,
-                },
-                {
-                    value: 'Day2',
-                    label: 'Day 2',
-                    disabled: true,
-                },
-                {
-                    value: 'Day3',
-                    label: 'Day 3'
-                },
-                ]},
-                {
-                value: 'Week2',
-                label: 'Week 2',
-                children: [
-                {
-                    value: 'Day1',
-                    label: 'Day 1',
-                },
-                {
-                    value: 'Day2',
-                    label: 'Day 2',
-                    disabled: true,
-                },
-                {
-                    value: 'Day3',
-                    label: 'Day 3',
-                    disabled: true,
-                },
-            ]}
-            ],
+    data(){
+        return{
+            radio1: ref('Week')
         }
-    },
-    methods: {
-      onSubmit() {
-        console.log(this.form.patientId);
-        console.log(this.form.day);
-      },
-      handleChange(value) {
-        console.log(value)
-      },
-      handleSelect(item) {
-        console.log(item);
-        this.form.patientId = item.id;
-      },
-      querySearch(queryString, cb) {
-        var ids = this.ids;
-        var results = queryString ? ids.filter(this.createFilter(queryString)) : ids;
-        // call callback function to return suggestions
-        cb(results);
-      },
-      createFilter(queryString) {
-        return (id) => {
-          return (id.id.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
-        };
-      },
-      loadAll() {
-        return [
-          { "id": "1111111" },
-          { "id": "2222222" },
-          { "id": "3333333" },
-          { "id": "4444444" },
-          { "id": "5555555" },
-          { "id": "6666666" },
-          { "id": "7777777" }
-         ];
-      },
-    },
-    beforeMount() {
-      this.ids = this.loadAll();
     }
 };
 </script>
