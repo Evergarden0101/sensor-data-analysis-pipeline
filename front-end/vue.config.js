96a078230c0aa27d349a91c2fba74433ed5403d2
epura <eleonora_pura@hotmail.it>
2023-11-23 11:31:32 +0100

    changed api target in config to avoid error

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -8,13 +8,14 @@ module.exports = defineConfig({
       },
       proxy: {
         '': {
-            target: 'http://127.0.0.1:5000',
+            target: 'http://127.0.0.1:5000/',
             // ws: true,
             changeOrigin: true
         },
         '^/api': {
-          target: 'http://localhost:8000'
-        }
+          target: 'http://10.28.51.5:8080/',
+          changeOrigin: true
+        },
       },
     },
   },
