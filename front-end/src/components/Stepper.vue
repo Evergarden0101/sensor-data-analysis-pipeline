ac44495547ef54690f783aabe597d6cc8e2f4a90
epura <eleonora_pura@hotmail.it>
2023-12-01 12:43:50 +0100

    changed stepper description

--- front-end/src/components/Stepper.vue
+++ front-end/src/components/Stepper.vue
@@ -1,9 +1,9 @@
 <template>
     <el-steps :active=step finish-status="process">
-        <el-step title="Patient Information" description="Provide the patient information" />
+        <el-step title="Patient Information" description="Select the desired patient" />
         <el-step title="Filtering" description="Filter out undesired intervals" />
-        <el-step title="Events Classification" description="Check for specific events in the data to label" />
-        <el-step title="Events Monitoring" description="Compare patients data between different months, weeks and days" />
+        <el-step title="Events Classification" description="Label events in the data" />
+        <el-step title="Treatment Analysis" description="Compare data between different days and weeks" />
     </el-steps>
 </template>
 
